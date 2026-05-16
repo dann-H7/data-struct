1b2bf532352dfdd1e916b6dd9f8f272aaf9a8032
KELVIN <dannhackshadow@gmail.com>
2026-05-16 11:44:11 +0100

    stack review - fix makfile and paths

--- stack/src/main.c
+++ stack/src/main.c
@@ -1,17 +1,17 @@
 /**
-*@file : main.c
-*@brief: the main file of stack implementation
-*/
+ * @file : main.c
+ * @brief: the main file of stack implementation
+ */
 
 #include <stdio.h>
-#include "..\include\stack.h"
+#include "../include/stack.h"
 
 int main(void){
     puts("\n[START OF PROGRAM]\n");
 
     Stacks pst = def_new_stacks();
     print_stacks(pst);
-    printf(">>> Longeur de la pile actuelle: %zu \n\n",
+    printf(">>> Stack length: %zu \n\n",
          high_stacks(pst));
 
     pst = push_stack(pst,  11);
@@ -19,17 +19,17 @@ int main(void){
     pst = push_stack(pst,  15);
     pst = push_stack(pst, 123);
     print_stacks(pst);
-    printf(">>> Longeur de la pile actuelle: %zu \n\n",
+    printf(">>> Stack length: %zu \n\n",
          high_stacks(pst));
 
     pst = pop_stack(pst);
     print_stacks(pst);
-    printf(">>> Longeur de la pile actuelle: %zu \n\n",
+    printf(">>> Stack length: %zu \n\n",
          high_stacks(pst));
 
     pst = free_stacks(pst);
     print_stacks(pst);
-    printf(">>> Longeur de la pile actuelle: %zu \n\n",
+    printf(">>> Stack length: %zu \n\n",
          high_stacks(pst));
     puts("\n[END  OF  PROGRAM]\n");
     return 0;
